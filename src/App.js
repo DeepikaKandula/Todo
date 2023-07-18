144b0fcbe4e95bad69c20de01accbd01dfaf4429
DeepikaKandula <119652894+DeepikaKandula@users.noreply.github.com>
2023-07-18 12:33:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import React from "react";
+import "./App.css";
+import Learn from "./components/learn";
+
+function App() {
+  return (
+    <div className="app">
+      <Learn/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/learn.js
@@ -0,0 +1,102 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./learn.css";
+
+function Todo({ todo, index, markTodo, removeTodo }) {
+  return (
+    <div
+      className="todo"
+      
+    >
+      <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.text}</span>
+      <div>
+        <Button variant="outline-success" onClick={() => markTodo(index)}>✓</Button>{' '}
+        <Button variant="outline-danger" onClick={() => removeTodo(index)}>✕</Button>
+      </div>
+    </div>
+  );
+}
+function FormTodo({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}> 
+    <Form.Group>
+      <Form.Label><b>Add Todo</b></Form.Label>
+      <Form.Control type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Add new todo" />
+    </Form.Group>
+    <div className='button d-flex justify-content-center' >
+    <Button variant="primary mb-3" type="submit">
+      Submit
+    </Button>
+    </div>
+  </Form>
+  );
+}
+
+function Learn(){
+  const [todos, setTodos] = React.useState([
+    {
+      text: "This is a sampe todo",
+      isDone: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const markTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+    return (
+        <>
+        {/* <div style={myStyle}> */}
+      <div class= 'login'>
+    <Card style={{ width: '40rem'}} className="border">
+      <Card.Body>
+        <Card.Title className='ti'>Todo List</Card.Title>
+        <FormTodo addTodo={addTodo} />
+        <div>
+          {todos.map((todo, index) => (
+            <Card>
+              <Card.Body>
+                <Todo
+                key={index}
+                index={index}
+                todo={todo}
+                markTodo={markTodo}
+                removeTodo={removeTodo}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </Card.Body> 
+    </Card>
+    </div>
+      </>
+  );
+}
+export default Learn;
+
